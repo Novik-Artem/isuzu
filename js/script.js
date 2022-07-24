bbbae2928231870d14f88c3b5c18113c0684c27b
conho-web <conho@internet.ru>
2022-07-24 15:07:39 +0300

    swap: - add block

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
 const screenWidth = window.screen.width;
 
 if (screenWidth <= 768) {
-  let swiper = new Swiper(".benefits__swiper", {
+  let swiperBenefits = new Swiper(".benefits__swiper", {
     slidesPerView: 1.3,
     spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".benefits-next",
+      prevEl: ".benefits-prev",
     },
+  });
+
+  let swiperSwap = new Swiper(".swap__swiper", {
+    slidesPerView: 1.3,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swap-next",
+      prevEl: ".swap-prev",
+    },
+    breakpoints: {
+      490: {
+        slidesPerView: 2,
+      },
+    }
+  });
+}
+
+else {
+  let swiperSwap = new Swiper(".swap__swiper", {
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swap-next",
+      prevEl: ".swap-prev",
+    },
+    breakpoints: {
+      1000: {
+        slidesPerView: 3,
+      },
+    }
   });
 }
